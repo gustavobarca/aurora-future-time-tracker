7b1d264295785490f61bdb9983471619e2fed4df
Leandro Teloli <leandroteloli@gmail.com>
2020-06-21 16:56:17 -0300

    FontAwesome Component adicionado e concurrently ajustado

--- src/components/ProjectsDropdown/index.js
+++ src/components/ProjectsDropdown/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import './styles.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
+import FontAwesomePro from '../FontAwesomePro';
 
 export default function ProjectsDropdown({ title }) {
   return (
     <div className="ProjectsDropdownContainer">
       <h2 className="text">{title}</h2>
-      <FontAwesomeIcon icon={faChevronDown} color="#FFFFFF" />
+      <FontAwesomePro icon="chevron-down" color="white"/>
     </div>
   );
 }
