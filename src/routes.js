f3b6fe31f1d2d79fed27019682e1a2ca6cc1ec08
Gustavo Barca <40965435+gustavobarca@users.noreply.github.com>
2021-01-13 20:43:25 -0300

    Revert "Revert "Adicionar projetos""

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import React from 'react';
 import App from './pages/App';
 import Settings from './pages/Settings';
 import AddProject from './pages/AddProject';
+import MapSections from './pages/MapSections';
 
 export default function Routes () {
   return (
@@ -10,7 +11,8 @@ export default function Routes () {
       <Switch>
         <Route exact path="/" component={App}/>
         <Route exact path="/settings" component={Settings}/>
-        <Route exact path="/projects/add" component={AddProject}/>         
+        <Route exact path="/add-project" component={AddProject}/>        
+        <Route exact path="/map-sections/:projectGid" component={MapSections}/>        
       </Switch>
     </BrowserRouter>
   )
